f7700e8329a5bae1a1d5fb3987a514f9fc5dd7a0
Nikita <nikitabalabuiev@gmail.com>
2019-03-03 18:49:58 +0100

    -Add player, player Movement, Map generation, Color palette, Actor class, actor interface

--- Rg/Game.cs
+++ Rg/Game.cs
@@ -4,73 +4,165 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using RLNET;
+using Rg.Core;
 
-public class Game
+public static class Game
 {
-    private static int playerX = 25;
-    private static int playerY = 25;
-    private static RLRootConsole rootConsole;
+
+
+    //public 
+
+    public static Player Player { get; private set; }
+    public static DungeonMap DungeonMap { get; private set; }
+    public static CommandSystem CommandSystem { get; private set; }
+
+    private static bool _renderRequired = true;
+
+
+    //private 
+
+
+    // The screen height and width are in number of tiles
+    private static readonly int _screenWidth = 100;
+    private static readonly int _screenHeight = 70;
+    private static RLRootConsole _rootConsole;
+
+    // The map console takes up most of the screen and is where the map will be drawn
+    private static readonly int _mapWidth = 80;
+    private static readonly int _mapHeight = 48;
+    private static RLConsole _mapConsole;
+
+    // Below the map console is the message console which displays attack rolls and other information
+    private static readonly int _messageWidth = 80;
+    private static readonly int _messageHeight = 11;
+    private static RLConsole _messageConsole;
+
+    // The stat console is to the right of the map and display player and monster stats
+    private static readonly int _statWidth = 20;
+    private static readonly int _statHeight = 70;
+    private static RLConsole _statConsole;
+
+    // Above the map is the inventory console which shows the players equipment, abilities, and items
+    private static readonly int _inventoryWidth = 80;
+    private static readonly int _inventoryHeight = 11;
+    private static RLConsole _inventoryConsole;
+
+
 
     public static void Main()
     {
+        CommandSystem = new CommandSystem();
+        Player = new Player();
+
+        MapGenerator mapGenerator = new MapGenerator(_mapWidth, _mapHeight);
+        DungeonMap = mapGenerator.CreateMap();
+
+        DungeonMap.UpdatePlayerFieldOfView();
+
         RLSettings settings = new RLSettings();
         settings.BitmapFile = "ascii_8x8.png";
         settings.CharWidth = 8;
         settings.CharHeight = 8;
-        settings.Width = 60;
-        settings.Height = 40;
+        settings.Width = _screenWidth;
+        settings.Height = _screenHeight;
         settings.Scale = 1f;
-        settings.Title = "RLNET Test";
-        settings.WindowBorder = RLWindowBorder.Resizable;
         settings.ResizeType = RLResizeType.ResizeCells;
+        settings.WindowBorder = RLWindowBorder.Fixed;
         settings.StartWindowState = RLWindowState.Normal;
+        settings.Title = "RougeSharp V3 Tutorial - Level 1";
 
-        rootConsole = new RLRootConsole(settings);
-        rootConsole.Update += rootConsole_Update;
-        rootConsole.Render += rootConsole_Render;
-        rootConsole.OnLoad += rootConsole_OnLoad;
-        rootConsole.Run();
+        _rootConsole = new RLRootConsole(settings);
 
+        _mapConsole = new RLConsole(_mapWidth, _mapHeight);
+        _messageConsole = new RLConsole(_messageWidth, _messageHeight);
+        _statConsole = new RLConsole(_statWidth, _statHeight);
+        _inventoryConsole = new RLConsole(_inventoryWidth, _inventoryHeight);
+
+        _rootConsole.Update += OnUpdate;
+        _rootConsole.Render += OnRender;
+        _rootConsole.OnLoad += OnLoad;
+
+
+        _mapConsole.SetBackColor(0, 0, _mapWidth, _mapHeight, Colors.FloorFov);
+
+        _messageConsole.SetBackColor(0, 0, _messageWidth, _messageHeight, Swatch.DbDeepWater);
+        _messageConsole.Print(1, 1, "Messages", Colors.TextHeading);
+
+        _statConsole.SetBackColor(0, 0, _statWidth, _statHeight, Swatch.DbOldStone);
+        _statConsole.Print(1, 1, "Stats", Colors.TextHeading);
+
+        _inventoryConsole.SetBackColor(0, 0, _inventoryWidth, _inventoryHeight, Swatch.DbWood);
+        _inventoryConsole.Print(1, 1, "Inventory", Colors.TextHeading);
+
+        _rootConsole.Run();
     }
 
-    static void rootConsole_OnLoad(object sender, EventArgs e)
+    static void OnLoad(object sender, EventArgs e)
     {
-        //rootConsole.SetWindowState(RLWindowState.Fullscreen);
+
+        //_rootConsole.SetWindowState(RLWindowState.Fullscreen);
     }
 
-    static void rootConsole_Update(object sender, UpdateEventArgs e)
+    static void OnUpdate(object sender, UpdateEventArgs e)
     {
 
-        RLKeyPress keyPress = rootConsole.Keyboard.GetKeyPress();
+
+        bool didPlayerAct = false;
+        RLKeyPress keyPress = _rootConsole.Keyboard.GetKeyPress();
+
         if (keyPress != null)
         {
             if (keyPress.Key == RLKey.Up)
-                playerY--;
+            {
+                didPlayerAct = CommandSystem.MovePlayer(Direction.Up);
+            }
             else if (keyPress.Key == RLKey.Down)
-                playerY++;
+            {
+                didPlayerAct = CommandSystem.MovePlayer(Direction.Down);
+            }
             else if (keyPress.Key == RLKey.Left)
-                playerX--;
+            {
+                didPlayerAct = CommandSystem.MovePlayer(Direction.Left);
+            }
             else if (keyPress.Key == RLKey.Right)
-                playerX++;
-            if (keyPress.Key == RLKey.Escape)
-                rootConsole.Close();
+            {
+                didPlayerAct = CommandSystem.MovePlayer(Direction.Right);
+            }
+            else if (keyPress.Key == RLKey.Escape)
+            {
+                _rootConsole.Close();
+            }
+        }
+
+        if (didPlayerAct)
+        {
+            _renderRequired = true;
         }
     }
 
-    static void rootConsole_Render(object sender, UpdateEventArgs e)
+    static void OnRender(object sender, UpdateEventArgs e)
     {
-        rootConsole.Clear();
-
-
-        rootConsole.SetChar(playerX, playerY, '@');
-
-        int color = 1;
-        if (rootConsole.Mouse.LeftPressed)
+        if (_renderRequired)
         {
-            color = 4;
-        }
-        rootConsole.SetBackColor(rootConsole.Mouse.X, rootConsole.Mouse.Y, RLColor.CGA[color]);
+            DungeonMap.Draw(_mapConsole);
+            Player.Draw(_mapConsole, DungeonMap);
+            //_rootConsole.Clear();
 
-        rootConsole.Draw();
+            // Blit the sub consoles to the root console in the correct locations
+
+            RLConsole.Blit(_mapConsole, 0, 0, _mapWidth, _mapHeight,
+              _rootConsole, 0, _inventoryHeight);
+            RLConsole.Blit(_statConsole, 0, 0, _statWidth, _statHeight,
+              _rootConsole, _mapWidth, 0);
+            RLConsole.Blit(_messageConsole, 0, 0, _messageWidth, _messageHeight,
+              _rootConsole, 0, _screenHeight - _messageHeight);
+            RLConsole.Blit(_inventoryConsole, 0, 0, _inventoryWidth, _inventoryHeight,
+              _rootConsole, 0, 0);
+
+            // Tell RLNET to draw the console that we set
+            _rootConsole.Draw();
+
+            _renderRequired = false;
+        }
     }
 }
